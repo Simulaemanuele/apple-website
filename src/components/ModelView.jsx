683fd1104d2c4065a128116e47b057ec66b556b0
Emanuele Simula <simulaemanuele@gmail.com>
2024-07-22 13:04:21 +0200

    feat: implemented ModelView.jsx successfully and edited IPhone.jsx which will allow to change texture and extend mesh changing color of the model. Created and implemented custom Loader component.

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,14 +1,16 @@
-import { PerspectiveCamera, View } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
 import React, { Suspense } from "react";
 import Lights from "./Lights";
 import IPhone from "./IPhone";
+import * as THREE from "three";
+import Loader from "./Loader";
 
 const ModelView = ({
   index,
   groupRef,
   gsapType,
   controlRef,
-  setRotationSize,
+  setRotationState,
   size,
   item,
 }) => {
@@ -16,9 +18,7 @@ const ModelView = ({
     <View
       index={index}
       id={gsapType}
-      className={`border-2 border-red-500 w-full h-full ${
-        index === 2 ? "right-[-100%]" : ""
-      }`}
+      className={`w-full h-full ${index === 2 ? "right-[-100%]" : ""}`}
     >
       /* Ambient Light*/
       <ambientLight intensity={0.3} />
@@ -26,9 +26,29 @@ const ModelView = ({
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
       /* Custom Lights*/
       <Lights />
-      <Suspense fallback={<div>Loading...</div>}>
-        <IPhone />
-      </Suspense>
+      /* Orbit Controls */
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+      <group
+        ref={groupRef}
+        name={`${index === 1 ? "small" : "large"}`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<Loader />}>
+          <IPhone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
     </View>
   );
 };
