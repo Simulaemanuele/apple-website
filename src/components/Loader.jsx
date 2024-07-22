f2d85df8f347c2ffd8f41fbf452b32034b7c79ae
Emanuele Simula <simulaemanuele@gmail.com>
2024-07-22 15:07:44 +0200

    feat: created Loader.jsx and implemented it in ModelView.jsx as a fallback

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,8 +4,11 @@ import React from "react";
 const Loader = () => {
   return (
     <Html>
-      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-        <div className="w-[10vw] h-[10vw] rounded-full">Loading...</div>
+      <div className="w-full h-full flex items-center justify-center">
+        <div
+          className="h-20 w-20 animate-spin rounded-full border-4 border-solid border-current border-r-transparent motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status"
+        />
       </div>
     </Html>
   );
