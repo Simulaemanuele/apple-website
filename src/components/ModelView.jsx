c01f55c37287574b51fe3bdec1339b67fc6380f8
Emanuele Simula <simulaemanuele@gmail.com>
2024-07-22 12:16:06 +0200

    feat: Implemented ModelView, created Lights.jsx and Translated model into react component IPhone.jsx and implemented into ModelView

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { PerspectiveCamera, View } from "@react-three/drei";
+import React, { Suspense } from "react";
+import Lights from "./Lights";
+import IPhone from "./IPhone";
 
-const ModelView = () => {
-  return <div>ModelView</div>;
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${
+        index === 2 ? "right-[-100%]" : ""
+      }`}
+    >
+      /* Ambient Light*/
+      <ambientLight intensity={0.3} />
+      /* Perspective Camera */
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      /* Custom Lights*/
+      <Lights />
+      <Suspense fallback={<div>Loading...</div>}>
+        <IPhone />
+      </Suspense>
+    </View>
+  );
 };
 
 export default ModelView;
